37e717ca6f4331c8fbb8b27f8c63685d6dd3ae31
Rakesh J Yadav <yad.rak@gmail.com>
2018-09-17 16:49:51 +0530

    Assignment 5 partial fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
 import { NavComponent } from './nav/nav.component';
 import { InputFormatDirective } from './input-format.directive';
+import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { InputFormatDirective } from './input-format.directive';
     PanelComponent,
     LikeComponent,
     NavComponent,
-    InputFormatDirective
+    InputFormatDirective,
+    ZippyComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/zippy/zippy.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'zippy',
+  templateUrl: './zippy.component.html',
+  styleUrls: ['./zippy.component.css']
+})
+export class ZippyComponent implements OnInit {
+
+  isClosed: Boolean = false;
+ 
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
